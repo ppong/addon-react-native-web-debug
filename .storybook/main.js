f2159a7202d0e3a061b2c2f394e2a5f71969eb2e
Peter Pong <ppong@Peters-MBP-2.attlocal.net>
2023-04-25 19:52:09 -0400

    debug reanimated bug

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-react-native-web',
+      options: {
+        modulesToTranspile: ['react-native-reanimated'],
+        babelPlugins: ['react-native-reanimated/plugin'],
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ stories/box.stories.tsx
@@ -0,0 +1,12 @@
+import { Box } from './box';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: 'Components/Box',
+  component: Box,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {},
+};
